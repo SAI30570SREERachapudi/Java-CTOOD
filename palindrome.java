7240703af8984617486a57e23ccea10291a52c44
RACHAPUDISAISREE <144793915+RACHAPUDISAISREE@users.noreply.github.com>
2024-02-05 22:11:06 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main{
+void ispalindrome (char[] s){
+int i,j,k,x,y,lengths;
+lengths=s.length;
+char[] b=new char[lengths];
+for(j=0;j<lengths;j++){
+b[j]=s[j];
+}
+x=0;
+y=lengths-1;
+while(x<y){
+char c=s[x];
+s[x]=s[y];
+s[y]=c;
+x++;
+y--;
+}
+for(k=0;k<lengths;k++){
+if(s[k]!=b[k]){
+System.out.println("Not a Palindrome");
+return;
+}
+}
+System.out.println("Palindrome");
+}
+public static void main(String args[]){
+String s;
+Scanner x=new Scanner(System.in);
+s=x.nextLine();
+x.close();
+char[] v=s.toCharArray();
