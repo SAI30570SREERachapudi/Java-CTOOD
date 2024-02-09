556aac68274b9d654a4a90018a77542ba5d78178
RACHAPUDI2005SAISREE <144793915+RACHAPUDISAISREE@users.noreply.github.com>
2024-02-09 22:40:12 +0530

    through command line

---
+++ print_strings.java
@@ -0,0 +1,12 @@
+package comandlinee;
+public class PRINT_STRINGS {
+
+	public static void main(String[] args) {
+int i=0;
+int n=args.length;
+for(i=0;i<n;i++) {
+System.out.print(args[i]+"-");
+}
+	}
+
+}
