0c43c82a0c415d247e32ae822a316b69f108d53a
SAI30570SREE@Rachapudi <144793915+RACHAPUDISAISREE@users.noreply.github.com>
2024-02-11 13:21:44 +0530

    Create Wrapper.java

---
+++ Wrapper.java
@@ -0,0 +1,23 @@
+package wrapperClass;
+
+public class WrapperClasss {
+
+	public static void main(String[] args) {
+	Integer oa=1;
+     Float of=1.2900909f;
+     Double od=2.909090900909009090990903;
+     Short os=19999;
+     Long ol=1229090909022222222l;
+     Boolean ob=true;
+     Byte obb=127;
+     System.out.println(oa);
+     System.out.println(od);
+     System.out.println(os);
+     System.out.println(of);
+     System.out.println(ol);
+     System.out.println(ob);
+     System.out.println(obb);
+
+	}
+
+}
