9506409898b8ef467b1579eb24fc660b65cf42b9
SAI30570SREE@Rachapudi <144793915+SAI30570SREERachapudi@users.noreply.github.com>
2024-02-16 22:36:14 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,14 +1,20 @@
 package practice;
-	public class stat {
-			static int n=5;
+
+import java.util.Scanner;
+
+public class Factorial {
+			static int n;
 			public static void main(String[] args) {
-			System.out.println("factorial is" +FactorialMethodClassLvlModularization.factorial());
+			Scanner k=new Scanner(System.in);
+			n=k.nextInt();
+			System.out.println("factorial is " +FactorialMethodClassLvlModularization.factorial(n));
+
 			}
 	}
 	class FactorialMethodClassLvlModularization {
-			static int r=stat.n;
+			static int r=Factorial.n;
 			static int x=1;
-			static int factorial() {
+			static int factorial(int r) {
 				while(r>1) {
 					
 					x=x*r;
