bf8fa0999781d46221ea6d9f8892403f743aac00
SAI30570SREE@Rachapudi <144793915+SAI30570SREERachapudi@users.noreply.github.com>
2024-02-17 23:44:30 +0530

    Create reverse a string.java

---
+++ reverse a string.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+  public static void main(String[] args) { 
+    String str = "Hello";
+    String reversedStr = "";
+
+    for(int i = str.length()-1; i >= 0; i--) {
+      reversedStr += str.charAt(i);
+    }
+    
+    System.out.println("Reversed string is: " + reversedStr); 
+  }
+}
