39518f373efecbf4e86030d935a9bffbc6e1382b
SAI30570SREE@Rachapudi <144793915+SAI30570SREERachapudi@users.noreply.github.com>
2024-02-22 18:24:26 +0530

    Create Reversed Right Angled Triangle.java

---
+++ Patterns in java/Reversed Right Angled Triangle.java
@@ -0,0 +1,19 @@
+package patternsinjava;
+
+public class ReversedRightAngledTriangle {
+public static void main(String arhs[]) {
+	for(int i=5;i>=0;i--) {
+		for(int j=1;j<=i;j++) {
+			System.out.print("* ");
+		}
+		System.out.print("\n");
+	}
+}
+}
+/*
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+  */
