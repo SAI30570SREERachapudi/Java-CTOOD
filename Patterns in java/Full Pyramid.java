3389ffecd618cafaf468bf654840c6295d4abd9d
SAI30570SREE@Rachapudi <144793915+SAI30570SREERachapudi@users.noreply.github.com>
2024-02-22 19:00:42 +0530

    Update Full Pyramid.java

--- Patterns in java/Full Pyramid.java
+++ Patterns in java/Full Pyramid.java
@@ -1,5 +1,6 @@
+package patternsinjava;
 public class FullPyramid {
-public static void main(String arhs[]) {
+public static void main(String args[]) {
 	int x=4,k;
 	for(int i=1;i<=5;i++) {
 		for(k=1;k<=x;k++) {
@@ -12,8 +13,8 @@ public static void main(String arhs[]) {
 		System.out.print("\n");
 	}
 x=1;
-	for(int i=5;i>=0;i--) {
-		for(k=1;k<x;k++) {
+	for(int i=4;i>=0;i--) {
+		for(k=0;k<x;k++) {
 			System.out.print(" ");
 		}
 		for(int j=1;j<=i;j++) {
