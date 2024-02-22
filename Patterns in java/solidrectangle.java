e5f3f9abeb8aa81a0976ccd38d8ec210d8600dcf
SAI30570SREE@Rachapudi <144793915+SAI30570SREERachapudi@users.noreply.github.com>
2024-02-22 17:56:10 +0530

    Create solidrectangle.java

---
+++ Patterns in java/solidrectangle.java
@@ -0,0 +1,12 @@
+package patternsinjava;
+
+public class Solid_rectangle {
+public static void main(String args[]) {
+	for(int i=0;i<5;i++) {
+		for(int j=0;j<5;j++) {
+			System.out.print("*");
+		}
+		System.out.print("\n");
+	}
+}
+}
