72ae2eb8d53184799f3f9ce6ae5484b488cf9edb
SAI30570SREE@Rachapudi <144793915+SAI30570SREERachapudi@users.noreply.github.com>
2024-02-22 18:16:19 +0530

    Create right angled triangle with spaces.java

---
+++ Patterns in java/right angled triangle with spaces.java
@@ -0,0 +1,22 @@
+public class Rightangledtriangle {
+	public static void main(String args[]) {
+		int x=4,k;
+		for(int i=1;i<=5;i++) {
+			for(k=1;k<=x;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print(j);
+			}
+			x--;
+			System.out.print("\n");
+}
+}}
+
+/*
+    1
+   12
+  123
+ 1234
+12345
+  */
