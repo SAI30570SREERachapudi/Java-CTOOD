3980f110b514c231ee8f03502a6b2bbff0c3f828
SAI30570SREE@Rachapudi <144793915+SAI30570SREERachapudi@users.noreply.github.com>
2024-02-25 20:46:51 +0530

    Create Number pyramid 1.java

---
+++ Patterns in java/Number pyramid 1.java
@@ -0,0 +1,15 @@
+package patternsinjava;
+
+public class Number_pyramid1 {
+public static void main(String args[]) {
+	int i,j;
+	for(i=5; i>=1; i--)
+	{
+		for(j=1; j<=i; j++)
+		{
+			System.out.print(j);
+		}
+		System.out.print("\n");
+	}
+}
+}
